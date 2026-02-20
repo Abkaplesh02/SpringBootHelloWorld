8a2db880e5ff130f38e8ee056ae589e59bf5d817
211924 <211924@ibsplc.com>
2026-02-20 15:29:16 +0530

    "updating project"

--- src/main/java/com/ibsplc/sprintboothelloworld/HomeController.java
+++ src/main/java/com/ibsplc/sprintboothelloworld/HomeController.java
@@ -1,23 +1,30 @@
 package com.ibsplc.sprintboothelloworld;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Value("${app.version:v1}")
+    public String version;
+
+    @Value("${app.message:Hello from DevOps JAVA App!}")
+    public String message;
+
     @GetMapping("/")
     public String home(){
-        return "Hello from DevOps JAVA App:) 🚀";
+        return message;
     }
 
     @GetMapping("/version")
     public String version(){
-            return "Version 1.0.0";
+            return "Version: "+ version;
         }
 
         @GetMapping("/health")
         public String health(){
-            return "Application is healthy!";
+            return "OK";
     }
 }
